b23230fdd37a1555ff2b217aabc755e31256a29b
Ine Ballardin <ineballardin@gmail.com>
2023-12-25 14:12:14 -0300

    add: main.go file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+
+	db2 "github.com/ineBallardin/go-hexagonal/adapters/db"
+	"github.com/ineBallardin/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", "db.sqlite")
+	productDbAdapter := db2.NewProductDb(db)
+	productService := application.NewProductService(productDbAdapter)
+	product, _ := productService.Create("Product Example", 30)
+
+	productService.Enable(product)
+
+}
